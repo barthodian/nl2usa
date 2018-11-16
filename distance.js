e137ef601c815ed00dc614c318376a8b7ba46231
Barthodian <barthodian@gmail.com>
2018-11-16 14:51:10 -0600

    Code cleaning

--- distance.js
+++ distance.js
@@ -3,9 +3,9 @@ const milimeter = {
     "foot": 304.8,
     "yard": 914.4,
     "mile": 1609340
-};
+}
 
-for (let i in milimeter) {
-    const distance = i.charAt(0).toUpperCase() + i.slice(1);
-    console.log("1", distance, "is equivalent to", milimeter[i], "milimeter,", milimeter[i]/10, "centimeter, and", milimeter[i]/1000, "meter");
+for ( let i in milimeter ) {
+    const distance = i.charAt( 0 ).toUpperCase() + i.slice( 1 )
+    console.log( "1", distance, "is equivalent to", milimeter[i], "milimeter,", milimeter[i]/10, "centimeter, and", milimeter[i]/1000, "meter" )
 }
