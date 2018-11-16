ebbd5737d4c4e7e19ea3ebe45af22af5b033b472
Bao <barthodian@gmail.com>
2018-11-15 20:34:59 -0600

    added Cheerio, Request and currency script

---
+++ currency.js
@@ -0,0 +1,16 @@
+const request = require("request");
+const cheerio = require("cheerio");
+
+request("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml", (error, response, body) => {
+  if (error){
+    console.log("error:", error);
+  }
+
+  if (statusCode =! 200){
+    console.log("statusCode:", response && response.statusCode);
+  }
+
+  const $ = cheerio.load(body);
+  const usd = $("Cube[currency='USD']").attr("rate");
+  console.log("'1 EUR is", usd, "USD' says European Central Bank.");
+});
