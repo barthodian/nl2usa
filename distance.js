f971e50e16079aa49a9176ed6e215673660cad39
Apple <apple@dhcp017104094121.corp.apple.com>
2018-11-17 14:58:45 -0600

    added function

--- distance.js
+++ distance.js
@@ -4,8 +4,30 @@ const milimeter = {
     "yard": 914.4,
     "mile": 1609340
 }
+const common_unit = {
+    "mm": 1,
+    "cm": 10,
+    "dm": 100,
+    "m": 1000,
+    "km": 1000000
+}
 
 for ( let i in milimeter ) {
     const distance = i.charAt( 0 ).toUpperCase() + i.slice( 1 )
     console.log( "1", distance, "is equivalent to", milimeter[i], "milimeter,", milimeter[i] / 10, "centimeter, and", milimeter[i] / 1000, "meter" )
 }
+
+function distanceConvert( distance, unit, conversion ) {
+    for ( let i in milimeter ) {
+        // const distance = i.charAt(0).toUpperCase() + i.slice(1)
+        if ( unit === i ) {
+            for ( let i in common_unit ) {
+                if ( conversion === i ) {
+                    console.log( conversion )
+                }
+            }
+        }
+    }
+}
+
+distanceConvert( 25, "inch", "cm" )
