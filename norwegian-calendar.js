d6b7c484610bbe1bbcb5cef3397189416c98eabd
Bao <barthodian@gmail.com>
2018-11-19 19:17:23 -0600

    date and details display

--- norwegian-calendar.js
+++ norwegian-calendar.js
@@ -5,16 +5,16 @@ function norwegianCalendar( origin, destination, date, currency ) {
     const url_calendar = `https://www.norwegian.com/api/fare-calendar/calendar?adultCount=1&destinationAirportCode=${ destination }&includeTransit=true&originAirportCode=${ origin }&outboundDate=${ date }&tripType=1&currencyCode=${ currency }&languageCode=nl-NL`
     
     request( url_calendar, ( error, response, body ) => {
-        if ( error ){
-          return "error:", error
-        }
+        if ( error ) return "error:", error
       
-        if ( statusCode =! 200 ){
-          return "statusCode:", response && response.statusCode
-        }
+        if ( statusCode =! 200 ) return "statusCode:", response && response.statusCode
       
         const destinations = JSON.parse( body )
-        console.log( destinations.outbound.days )
+        
+        for ( let i = 0; i < destinations.outbound.days.length; i++ ) {
+            let item = destinations.outbound.days[i]
+            console.log( `${ item.date } ${ currency } ${ item.displayPrice } has ${ item.transitCount } transit count and is sold out: ${ item.isSoldOut }` )
+        }
     } )
 }
 
